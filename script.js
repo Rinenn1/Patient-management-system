826e4fd12c6042b76eba036627f91c11a0ff852c
Isaac <bennisaac24@gmail.com>
2024-12-19 12:10:20 +0300

    accessed the inputs in the DOM and changed the id attributes for the input areas

---
+++ script.js
@@ -0,0 +1,21 @@
+//get the input values and list values
+const searchInput = document.getElementById('searchInput');
+const patientList = document.getElementById('generalPatients');
+const patientName = document.getElementById('nameInput');
+const patientAge = document.getElementById('ageInput');
+const genderInput = document.getElementById('genders');
+const phoneNumber = document.getElementById('phoneNumberInput');
+const emailAddress = document.getElementById('emailInput');
+const homeAddress = document.getElementById('homeAddressInput');
+const diagnosis = document.getElementById('diagnosisInput');
+const admissionDate = document.getElementById('admissionDateInput');
+const followUpDate = document.getElementById('followUpDateInput');
+const dischargeDate = document.getElementById('dischargeDateInput');
+const emergencyName = document.getElementById('emergencyNameInput');
+const relationship = document.getElementById('emergencyRelationInput');
+const emergencyTel = document.getElementById('emergencyPhoneInput');
+const insuranceProvider = document.getElementById('insuranceProviderNameInput');
+const policyNumber = document.getElementById('insurancePolicyNumberInput');
+const submitButton = document.querySelector('#button');
+
+
